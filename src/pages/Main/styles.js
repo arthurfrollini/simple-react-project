253d55f49e62f9b495324834eac1403c94cbe8e8
= <=>
2021-05-27 14:46:09 -0300

    Primeiro Commit

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,79 @@
+import styled, { keyframes, css } from 'styled-components';
+
+export const Form = styled.form`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+
+  input {
+    flex: 1;
+    border: 1px solid #eee;
+    padding: 10px 15px;
+    border-radius: 4px;
+    font-size: 16px;
+  }
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg)
+  }
+`;
+
+export const SubmitButton = styled.button.attrs((props) => ({
+  type: 'submit',
+  disabled: props.loading,
+}))`
+  background: linear-gradient(45deg, #fd297b, #ff5864, #ff655b);
+  border: 0;
+  padding: 0 15px;
+  margin-left: 10px;
+  border-radius: 4px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
+  ${(props) =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${rotate} 2s linear infinite;
+      }
+    `}
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #ff5864;
+      text-decoration: none;
+    }
+  }
+`;
+
+/* Verifica se existe um li antes dele, caso não,
+ele não aplica o estilo no primeiro: & + li, ou
+seja, aplica a estilização em todos os itens,
+menos no primeiro */
